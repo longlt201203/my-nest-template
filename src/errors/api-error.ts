3d3c6537d99aa4a3f516b3b55b924e009fab475a
Le Thanh Long <longlt201203@gmail.com>
2024-06-29 14:22:46 +0700

    feat: 🎸 update

--- src/errors/api-error.ts
+++ src/errors/api-error.ts
@@ -1,9 +1,11 @@
 export class ApiError<T = any> {
-    code: string;
-    message: string;
-    detail: T;
+	code: string;
+	message: string;
+	detail: T;
+	status?: number;
 
-    constructor(props: ApiError) {
-        Object.assign(this, props);
-    }
+	constructor(props: ApiError) {
+		Object.assign(this, props);
+		if (!this.status) this.status = 400;
+	}
 }
--- src/errors/api-validation-error.ts
+++ src/errors/api-validation-error.ts
@@ -1,12 +1,15 @@
 import { ValidationError } from "class-validator";
 import { ApiError } from "./api-error";
 
-export class ApiValidationError extends ApiError<ValidationError[]> {
-    constructor(errors: ValidationError[]) {
-        super({
-            code: "validation_err",
-            message: "Validation Error!",
-            detail: errors
-        });
-    }
+export class ApiValidationError extends ApiError {
+	constructor(errors: ValidationError[]) {
+		super({
+			code: "validation_err",
+			message: "Validation Error!",
+			detail: errors.map((err) => ({
+				field: err.property,
+				constraints: Object.keys(err.constraints),
+			})),
+		});
+	}
 }
