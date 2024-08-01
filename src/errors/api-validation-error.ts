85ac32d58b60df272bf4d86f5487242789060b8b
longthanhle1122 <longlt201203@gmail.com>
2024-08-01 15:55:24 +0700

    fix: 🐛 api error
    c

--- src/errors/api-validation-error.ts
+++ src/errors/api-validation-error.ts
@@ -3,9 +3,10 @@ import { ApiError } from "./api-error";
 
 export class ApiValidationError extends ApiError<ValidationError[]> {
     constructor(errors: ValidationError[]) {
-        super();
-        this.code = "validation_err";
-        this.message = "Validation Error!";
-        this.detail = errors;
+        super({
+            code: "validation_err",
+            message: "Validation Error!",
+            detail: errors
+        });
     }
 }
