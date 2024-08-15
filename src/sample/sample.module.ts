8039212faeeed0ab12ed965abad7130cf842d9d1
Le Thanh Long <longlt201203@gmail.com>
2024-08-15 16:34:01 +0700

    Update

--- src/sample/sample.module.ts
+++ src/sample/sample.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SampleService } from './sample.service';
 import { SampleController } from './sample.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Sample } from './entities/sample.entity';
 
 @Module({
   controllers: [SampleController],
   providers: [SampleService],
+  imports: [TypeOrmModule.forFeature([Sample])]
 })
 export class SampleModule {}
