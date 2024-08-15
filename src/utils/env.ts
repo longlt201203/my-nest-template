ce72fe4dc43878994a270985eda5695972ce1b8d
Le Thanh Long <longlt201203@gmail.com>
2024-08-15 16:50:17 +0700

    update

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -10,6 +10,8 @@ export const Env = {
 	DB_NAME: process.env.DB_NAME || "",
 	DB_USER: process.env.DB_USER || "",
 	DB_PASS: process.env.DB_PASS || "",
+
+	ENABLE_SWAGGER: (process.env.ENABLE_SWAGGER || "false") == "true",
 } as const;
 
 console.log(Env);
