47db6b0d2e9f2bbfe4f30bbbbd42b3a048958d25
Le Thanh Long <longlt201203@gmail.com>
2024-08-15 16:50:17 +0700

    update

--- src/db/db.module.ts
+++ src/db/db.module.ts
@@ -1,8 +1,13 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { datasource } from "./datasource";
+import { addTransactionalDataSource } from "typeorm-transactional";
 
 @Module({
-	imports: [TypeOrmModule.forRoot(datasource.options)],
+	imports: [
+		TypeOrmModule.forRootAsync({
+			dataSourceFactory: async () => addTransactionalDataSource(datasource),
+		}),
+	],
 })
 export class DbModule {}
