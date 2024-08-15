058b19be228a14deaf5dbea82c536b86ef9ade24
Le Thanh Long <longlt201203@gmail.com>
2024-08-15 16:34:02 +0700

    feat: 🎸 update

--- src/utils/my-exception.filter.ts
+++ src/utils/my-exception.filter.ts
@@ -1,5 +1,11 @@
 import { ApiError } from "@errors";
-import { ExceptionFilter, ArgumentsHost, HttpStatus, InternalServerErrorException, HttpException, Catch } from "@nestjs/common";
+import {
+	ExceptionFilter,
+	ArgumentsHost,
+	InternalServerErrorException,
+	HttpException,
+	Catch,
+} from "@nestjs/common";
 import { Response } from "express";
 
 /**
@@ -8,30 +14,37 @@ import { Response } from "express";
  */
 @Catch()
 export class MyExceptionFilter implements ExceptionFilter {
-    /**
-     * Handles the thrown exception.
-     * 
-     * @param exception The thrown exception.
-     * @param host The arguments host providing access to the request/response objects.
-     */
-    catch(exception: any, host: ArgumentsHost) {
-        const res = host.switchToHttp().getResponse<Response>();
-        let status = HttpStatus.INTERNAL_SERVER_ERROR;
-        let data: ApiError<any> = { code: InternalServerErrorException.name, message: "Internal Server Error", detail: null };
+	/**
+	 * Handles the thrown exception.
+	 *
+	 * @param exception The thrown exception.
+	 * @param host The arguments host providing access to the request/response objects.
+	 */
+	catch(exception: any, host: ArgumentsHost) {
+		const res = host.switchToHttp().getResponse<Response>();
+		let data: ApiError<any> = {
+			code: InternalServerErrorException.name,
+			message: "Internal Server Error",
+			detail: null,
+			status: 500,
+		};
 
-        // Handle specific exception types and extract relevant information.
-        if (exception instanceof ApiError) {
-            status = HttpStatus.BAD_REQUEST;
-            data = exception;
-        } else if (exception instanceof HttpException) {
-            status = exception.getStatus();
-            data = { code: exception.constructor.name, message: exception.message, detail: exception.getResponse() }
-        } else {
-            // Log unexpected errors for debugging.
-            console.error(exception);
-        }
-        
-        // Send the formatted error response.
-        res.status(status).send(data);
-    }
-} 
+		// Handle specific exception types and extract relevant information.
+		if (exception instanceof ApiError) {
+			data = exception;
+		} else if (exception instanceof HttpException) {
+			data = {
+				code: exception.constructor.name,
+				message: exception.message,
+				detail: exception.getResponse(),
+				status: exception.getStatus(),
+			};
+		} else {
+			// Log unexpected errors for debugging.
+			console.error(exception);
+		}
+
+		// Send the formatted error response.
+		res.status(data.status).send(data);
+	}
+}
